a2cd414de5584056741d529817977fa217b21fd8
Stephen <tait.stephen.p@gmail.com>
2019-03-03 00:41:30 +1100

    Added coin pickups and basic player inventory
    player inventory currently only tracks money from coin pickups

---
+++ Assets/PlayerInventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public int money;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PickupCoin>())
+        {
+            money += other.GetComponent<PickupCoin>().value;
+            Destroy(other.gameObject);
+        }   
+    }
+}

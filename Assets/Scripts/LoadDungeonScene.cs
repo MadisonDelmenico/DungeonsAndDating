a189f6136f1fb41c1dbbe170f926666701eff836
Stephen <tait.stephen.p@gmail.com>
2019-03-12 12:43:05 +1100

    Unity Scene Cleanup

--- Assets/Scripts/LoadDungeonScene.cs
+++ Assets/Scripts/LoadDungeonScene.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadDungeonScene : MonoBehaviour {
+public class LoadDungeonScene : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     public void LoadDungeon()
     {
-        SceneManager.LoadScene("DungeonGreybox");
+        SceneManager.LoadScene("Dungeon_Madison_2");
     }
 }

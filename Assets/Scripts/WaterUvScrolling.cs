f717023b06146ba677bb57e95612f14be05f704b
michelle <1010199@student.sae.edu.au>
2019-03-29 16:13:39 +1100

    Cleaned up unity folders, made materials - Michelle

---
+++ Assets/Scripts/WaterUvScrolling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WaterUvScrolling : MonoBehaviour
+{
+
+    public int materialIndex = 0;
+    public Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
+    public string textureName = "_MainTex";
+
+    Vector2 uvOffset = Vector2.zero;
+
+    void LateUpdate()
+    {
+        uvOffset += (uvAnimationRate * Time.deltaTime);
+        if (GetComponent<Renderer>().enabled)
+        {
+            GetComponent<Renderer>().materials[materialIndex].SetTextureOffset(textureName, uvOffset);
+        }
+    }
+}
+

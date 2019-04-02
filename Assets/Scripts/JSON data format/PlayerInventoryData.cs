9841b7a7b6a0cb716629d66b4a3e527bb7d72b73
Stephen <tait.stephen.p@gmail.com>
2019-04-02 11:40:03 +1100

    Adding JSON PlayerData Saving

---
+++ Assets/Scripts/JSON data format/PlayerInventoryData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerInventoryData
+{
+    public int money;
+
+    [Header("Treasure")]
+    public int commonTreasure;
+    public int uncommonTreasure;
+    public int rareTreasure;
+
+    [Header("Tier 1 Gifts")]
+    public int steakOfStrength;
+    public int snakeShapedWand;
+    public int holyOrderEnlistment;
+
+    [Header("Tier 2 Gifts")]
+    public int halberdWhetstone;
+    public int spikedBoots;
+    public int rangerScoutCookies;
+
+    [Header("Tier 3 Gifts")]
+    public int scaleRepairKit;
+    public int hauntedDagger;
+    public int localCharityReciept;
+}

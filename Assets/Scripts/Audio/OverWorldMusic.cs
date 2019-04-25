0f0c5ffeed182084b1f6e39a300bbf3045069eea
Joel Davies <joel@jdav.studio>
2019-04-25 22:39:37 +1000

    Updated overworld music

---
+++ Assets/Scripts/Audio/OverWorldMusic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverWorldMusic : MonoBehaviour
+{
+    public Transform player;
+    public Transform inn;
+    private FMOD.Studio.EventInstance overworldMusic;
+    private float distanceToInn;
+    private float innnumber;
+
+    private void Start()
+    {
+        overworldMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music/v1/Music_Overworld");
+        overworldMusic.start(); //Overworld Music
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Environment/Env_Overworld_Atmos", this.transform.position); // ROOMTONE
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToInn = Vector3.Distance(inn.position, player.position);
+        //Debug.Log(distanceToInn);
+        overworldMusic.setParameterByName("DistanceToInn", distanceToInn);
+    }
+
+    private void OnDestroy()
+    {
+        overworldMusic.release();
+    }
+}

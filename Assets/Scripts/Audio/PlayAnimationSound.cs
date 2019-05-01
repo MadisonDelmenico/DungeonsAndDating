a438ce5409125e03a42ccb8d5464fd144f3369da
Joel Davies <joel@jdav.studio>
2019-05-02 03:10:26 +1000

    Updated banks

---
+++ Assets/Scripts/Audio/PlayAnimationSound.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimationSound : MonoBehaviour
+{
+    public void PlaySound(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+    }
+}

ef49bdda5d7a4ed6c465297769162ae8ae71e82e
Joel Davies <joel@jdav.studio>
2019-05-02 13:25:33 +1000

    Adjust levels, edited character creation audio

---
+++ Assets/Scripts/Audio/DungeonAudio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonAudio : MonoBehaviour
+{
+    public GameObject mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Music/v1/Music_Dungeon", mainCamera);
+        FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Environment/Env_RoomTone_Dungeon", mainCamera);
+    }
+}
